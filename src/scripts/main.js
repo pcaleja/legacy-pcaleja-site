9d149489e7701ac6ee08f1203064bffbe0c9c3ec
Philip Caleja <pcaleja@gmail.com>
2016-10-17 20:45:20 -0400

    improve anchor scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,6 @@
+import _ from 'lodash';
+
+// Header
 const body = document.querySelector('body');
 const headerToggle = document.querySelector('.header__toggle');
 let scrollPosition = 0;
@@ -14,4 +17,23 @@ function toggleHeaderOff() {
 
 headerToggle.addEventListener('click', () => {
   return body.classList.value === '' ? toggleHeaderOn() : toggleHeaderOff();
+});
+
+
+// Anchor Scroll
+const anchorLink = document.querySelectorAll('a');
+_.each(anchorLink, (val) => {
+  if (val.hash) {
+    val.addEventListener('click', (e) => {
+      e.preventDefault();
+      const source = val;
+      const target = document.querySelector(val.hash);
+      const sourceOffset = source.offsetTop;
+      const targetOffset = target.offsetTop;
+      const equalizer = 0.1;
+      const sourceEqualized = sourceOffset * equalizer;
+      const offsetDiff = Math.abs(targetOffset - sourceEqualized);
+      window.scrollTo(0, offsetDiff);
+    });
+  }
 });

7bb71feb686a125c5e09450fdd2b889f01238cf4
Philip Caleja <pcaleja@gmail.com>
2016-10-17 21:44:42 -0400

    fix mobile anchor links

--- src/scripts/components/header-toggle.js
+++ src/scripts/components/header-toggle.js
@@ -1,16 +1,51 @@
+import _ from 'lodash';
+import anchorScroll from './anchor-scroll';
+
 const body = document.querySelector('body');
+const links = document.querySelectorAll('.header a');
 const toggle = document.querySelector('.header__toggle');
+const targetPosition = {};
 let scrollPosition = 0;
 
 const headerToggle = {
+  setTargetPostion() {
+    _.each(links, (link) => {
+      if (link.hash) {
+        const target = document.querySelector(link.hash);
+        targetPosition[link.hash.replace('#', '')] = target.offsetTop;
+      }
+    });
+  },
+
+  setMobileAnchorLinks() {
+    _.each(links, (link) => {
+      if (link.hash) {
+        link.addEventListener('click', (e) => {
+          e.preventDefault();
+          const source = link;
+          const sourceOffset = source.offsetTop;
+          body.classList = '';
+          const targetOffset = targetPosition[link.hash.replace('#', '')];
+          const equalizer = 0.1;
+          const sourceEqualized = sourceOffset * equalizer;
+          const offsetDiff = Math.abs(targetOffset - sourceEqualized - 10);
+          window.scrollTo(0, offsetDiff);
+        });
+      }
+    });
+  },
+
   on() {
     scrollPosition = window.scrollY;
+    headerToggle.setTargetPostion();
     body.classList = 'nav-active';
+    headerToggle.setMobileAnchorLinks();
   },
 
   off() {
     body.classList = '';
     window.scrollTo(0, scrollPosition);
+    anchorScroll.init();
   },
 
   init() {

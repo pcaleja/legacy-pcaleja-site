0036c0c8f50278fefcaa91202a165e791d0df583
Philip Caleja <pcaleja@gmail.com>
2016-10-19 23:14:20 -0400

    setup studies

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
+import 'codemirror/mode/javascript/javascript';
+import codeMirror from 'codemirror';
 import headerToggle from './components/header-toggle';
 import anchorScroll from './components/anchor-scroll';
 
 headerToggle.init();
 anchorScroll.init();
+
+const editor = codeMirror(document.querySelector('.studies__test'), {
+  value: 'function myScript(){return 100;};\nmyScript();',
+  mode: 'javascript',
+  lineNumbers: true,
+  tabSize: 2,
+  lineWrapping: true,
+});
